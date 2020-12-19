8881691d0555c67abbad15d9bebf949a9a609769
nir <nirp010@gmail.com>
2020-12-19 14:44:52 +0200

    תיקון שיהיה ג'נרי ללא שמות יחודים לאלמנטים ושכפול js מתאים למספר לא מוגדר של הראה עוד

--- app/src/main/assets/html/js/MainAppJS.js
+++ app/src/main/assets/html/js/MainAppJS.js
@@ -24,48 +24,18 @@ function updateHeaders(num1, num2, num3) {
 
 }
 
-function readMoreFunction() {
-  var dots = document.getElementById("dots");
-  var dots2 = document.getElementById("dots2");
-  var moreText = document.getElementById("more");
-  var moreText2 = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn");
-  var btnText2 = document.getElementById("myBtn2");
+function readMoreFunction(event) {
+  var target = event.target;
+  var more =  target.previousElementSibling;
+  var dots = more.previousElementSibling;
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
-    btnText.innerHTML = "קרא עוד";
-    moreText.style.display = "none";
+    target.innerHTML = "קרא עוד";
+    more.style.display = "none";
     } else {
     dots.style.display = "none";
-    btnText.innerHTML = "הסתר";
-    moreText.style.display = "inline";
-    }
-  if (dots2.style.display === "none") {
-      dots2.style.display = "inline";
-      btnText2.innerHTML = "קרא עוד";
-      moreText2.style.display = "none";
-    } else {
-      dots2.style.display = "none";
-      btnText2.innerHTML = "הסתר";
-      moreText2.style.display = "inline";
-    }
-}
-function readMoreFunction2() {
-
-  var dots2 = document.getElementById("dots2");
-
-  var moreText2 = document.getElementById("more2");
-
-  var btnText2 = document.getElementById("myBtn2");
-
-   if (dots2.style.display === "none") {
-      dots2.style.display = "inline";
-      btnText2.innerHTML = "קרא עוד";
-      moreText2.style.display = "none";
-    } else {
-      dots2.style.display = "none";
-      btnText2.innerHTML = "הסתר";
-      moreText2.style.display = "inline";
+    target.innerHTML = "הסתר";
+    more.style.display = "inline";
     }
 }

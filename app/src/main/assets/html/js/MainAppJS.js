153a2e38f953e4c643fcdc00bec096e83cbe9a86
nir <nirp010@gmail.com>
2020-12-19 14:12:20 +0200

    clean old java scripts for tabs or collision

--- app/src/main/assets/html/js/MainAppJS.js
+++ app/src/main/assets/html/js/MainAppJS.js
@@ -1,6 +1,4 @@
-window.onscroll = function() {myFunction()};
 var header = document.getElementById("myHeader");
-
 images = document.getElementsByTagName("img");
 AndroidCallback.getImageName(images[0].src);
 
@@ -23,34 +21,6 @@ function updateHeaders(num1, num2, num3) {
          }
 
 }
-
-
-var sticky = header.offsetTop;
-function myFunction() {
-  var scrollTop =  window.pageYOffset;
-  if (scrollTop > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-buttons = document.getElementsByClassName("tablinks");
-buttons[0].click();
 
 function readMoreFunction() {
   var dots = document.getElementById("dots");
